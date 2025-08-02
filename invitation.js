c14e0f5e30ac311ddc44cf4bc093ef4f9a4e46b0
CDenson86 <cedric.denson1986@gmail.com>
2025-08-01 20:57:32 -0500

    Add files via upload

---
+++ invitation.js
@@ -0,0 +1,28 @@
+
+//Function to replace the content and show output area. Called from HTML.
+function replaceContent() {
+	//declaring the variables
+	let myRecipientName;
+	let myHostName; // Added: declare variable for host name
+	
+	//setting the variable to the input field's id named recipientNameInput's value
+	myRecipientName = document.getElementById("recipient-input").value;
+	
+	// Added: read the entry in the host-input field and store it in the host name variable
+	myHostName = document.getElementById("host-input").value;
+
+	//logging to devtools console
+	console.log('Variable myRecipientName: ' + myRecipientName);
+	
+	// Added: log the host name variable to the console
+	console.log('Variable myHostName: ' + myHostName);
+	
+	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
+	document.getElementById("recipient-placeholder").innerHTML = myRecipientName;
+
+	// Added: replace the host-placeholder text with the host's name
+	document.getElementById("host-placeholder").innerHTML = myHostName;
+
+	//make output area visible by removing hidden class
+	document.getElementById("outputArea").classList.remove("hidden");
+}
